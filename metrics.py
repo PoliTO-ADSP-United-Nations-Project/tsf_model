e0c2c5db73fe76ca31e73ffbd1cc9b06306ee2c2
Valerio Zingarelli <39964860+z216z@users.noreply.github.com>
2023-02-01 17:28:41 +0100

    MAE added to metrics

--- metrics.py
+++ metrics.py
@@ -2,3 +2,8 @@ import numpy as np
 def mape(actual, pred): 
     actual, pred = np.array(actual), np.array(pred)
     return np.mean(np.abs((actual - pred) / actual)) * 100
+
+def mae(actual, pred):
+    forecast_errors = [actual[i]-pred[i] for i in range(len(actual))]
+    mean_absolute_error = np.mean( np.abs(forecast_errors) )
+    return mean_absolute_error
